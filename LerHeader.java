e9613df3bf9f5c1895a86ff5657c629c9dd08cf7
Lucas Augusto Hartmann <78509840+lucashartmann@users.noreply.github.com>
2022-05-27 20:47:52 -0300

    Update LerHeader.java

--- LerHeader.java
+++ LerHeader.java
@@ -1,17 +1,27 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-
+import java.util.Scanner;
 
 
 public class LerHeader {
     public static void main(String[] args) {
+        
         String file = "TA_PRECO_MEDICAMENTO.csv";
         String headers;
         String line;
+        String nome;
+
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            Scanner sc = new Scanner(System.in);
+               System.out.println("\nDigite o nome do rémedio:\n");
+               nome = sc.nextLine();
             headers = br.readLine();
             while((line = br.readLine()) != null){
-               System.out.println("\n"+line);
+        
+                if(line.contains(nome.toUpperCase())) {
+                    System.out.println("\n"+line);
+                }
+             
             }
         } catch (Exception e){
             System.out.println(e);
